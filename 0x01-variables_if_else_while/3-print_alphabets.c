edb73835a597819159335d22f75777c45b08745c
Mistaken300 <motiongraph98@gmail.com>
2023-06-22 12:34:24 -0700

    correction7

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,25 +1,24 @@
-#include <stdio.h>
+#include <time.h>
+
 
 /**
  * main - Entry point
- *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
+
 {
-int lowerCase = 'a';
-int upperCase = 'A';
-while (lowerCase <= 'z')
-{
-putchar(lowerCase);
-lowerCase += 1;
-}
-while (upperCase <= 'z')
-{
-putchar(upperCase);
-upperCase += 1;
-}
-putchar('\n');
-return (0);
+
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	putchar(low);
+
+	for(low = 'A'; low <= 'Z'; low++)
+	putchar(low);
+	putchar('\n');
+
+	return (0);
+
 }
