c08b590854f0019929376038d709eaf12529a53e
Mistaken300 <motiongraph98@gmail.com>
2023-07-11 17:45:44 -0700

    correction

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,12 +1,12 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
  * create_array - create array of size size and assign char c
  * @size: size of array
  * @c: char to assign
  * Description: creat array of size size and assign char c
  * Return: pointer to array, NULL if fail
- *
  */
 char *create_array(unsigned int size, char c)
 {
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0 || str == NULL)
 		return (NULL);
 
-	for (i = 0, 1 < size; i++)
+	for (i = 0; i < size; i++)
 		str[i] = c;
 	return (str);
 }
